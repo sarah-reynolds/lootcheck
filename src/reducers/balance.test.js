1941d6963e5ce2d9335c715e0fd47d97dbb50355
Sarah Basinger <sarrey@gmail.com>
2018-03-09 15:22:17 -0500

    Initial commit. App setup, initial balance action and reducer, plus corresponding tests

---
+++ src/reducers/balance.test.js
@@ -0,0 +1,27 @@
+import balanceReducer from './balance'
+import * as constants from '../actions/constants'
+
+describe('balanceReducer', () => {
+	it('sets a balance', () => {
+		const balance = 10
+
+		expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
+		.toEqual(balance)
+	})
+
+	it('deposits into the balance', () => {
+		const deposit = 10
+		const initialState = 5
+
+		expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit}))
+			.toEqual(initialState + deposit)
+	})
+
+	it('withdraws from the balance', () => {
+		const withdrawal = 10
+		const initialState = 20
+
+		expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdrawal}))
+			.toEqual(initialState - withdrawal)
+	})
+})
